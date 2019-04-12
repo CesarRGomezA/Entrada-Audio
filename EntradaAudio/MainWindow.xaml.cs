9d676186043287ee393fde7f0d314834d14bcd8b
CesarRGomezA <CesarRGomezA@outlook.com>
2019-04-12 13:53:11 -0500

    Vacacioneeeeeeeeeesssssss

--- EntradaAudio/MainWindow.xaml.cs
+++ EntradaAudio/MainWindow.xaml.cs
@@ -64,7 +64,24 @@ namespace EntradaAudio
             byte[] buffer = e.Buffer;
             int bytesGrabados = e.BytesRecorded;
             float acumulador = 0.0f;
-            for (int i=0; i<bytesGrabados; i+=2)
+
+            double numeroDeMuestras = bytesGrabados / 2;
+            int exponente = 1;
+            int numeroDeMuestrasComplejas = 0;
+            int bitsMaximos = 0;
+
+            do
+            {
+                bitsMaximos = (int)Math.Pow(2, exponente);
+                exponente++;
+            } while (bitsMaximos < numeroDeMuestras);
+
+            numeroDeMuestrasComplejas = bitsMaximos / 2;
+            exponente--;
+
+            Complex[] señalCompleja = new Complex[numeroDeMuestrasComplejas];
+
+            for (int i = 0; i < bytesGrabados; i += 2)
             {
                 //Transformando 2 bytes separados
                 //en una muestra de 16 bits
@@ -76,9 +93,18 @@ namespace EntradaAudio
                 float muestra32bits = (float)muestra / 32768.0f;
                 acumulador += Math.Abs(muestra32bits);
 
+                if (i/2 < numeroDeMuestrasComplejas)
+                {
+                    señalCompleja[i / 2].X = muestra32bits;
+                }
+
             }
             float promedio = acumulador / (bytesGrabados / 2.0f);
             sldMicrofono.Value = (double)promedio;
+
+
+            //FastFourierTransform.FFT()
+
         }
 
         private void BtnDetener_Click(object sender, RoutedEventArgs e)
